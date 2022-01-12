042b26dfb2943d94074dc49faaa25459efc69209
nakamarusun <nakamarujc@gmail.com>
2022-01-13 00:48:20 +0700

    Added docker compose dev file for inserting dev google key into container. Succesfully tested first upload into google cloud storage

--- streamer/src/convert.sh
+++ streamer/src/convert.sh
@@ -2,6 +2,12 @@
 # Converts a flv file from nginx rtmp to mp4 and deletes the original
 fname=$1
 
+if [ -z "$fname" ]
+then
+  echo "Insert a filename!"
+  exit 1
+fi
+
 # Transcode video to mp4
 ffmpeg -i ${fname} -b:v 1500k -vcodec copy -acodec copy ${fname%.*}.mp4 </dev/null
 
@@ -9,4 +15,4 @@ ffmpeg -i ${fname} -b:v 1500k -vcodec copy -acodec copy ${fname%.*}.mp4 </dev/nu
 rm ${fname}
 
 # Return the name of the file
-echo ${fname%/*}/${fname%.*}.mp4
+echo ${fname%.*}.mp4
--- streamer/src/index.js
+++ streamer/src/index.js
@@ -2,9 +2,19 @@ require('dotenv').config()
 
 const express = require('express')
 const shelljs = require("shelljs")
+const path = require("path")
+const { Storage } = require('@google-cloud/storage');
 
+// Express app
 const app = express()
 const port =  process.env.PORT || 80
+
+// Google bucket object
+const storage = new Storage();
+const bucket = storage.bucket(process.env.GCLOUD_STORAGE_NAME);
+
+// Video directory
+const recPath = "tmp/rec/";
 
 // Authenticates the user and checks whether the user is allowed to record
 app.get('/on_publish', (req, res) => {
@@ -22,25 +32,36 @@ app.get('/on_play', (req, res) => {
 })
 
 // Saves the video and sends it to google bucket
-app.get("save_video", (req, res) => {
+app.get("/save_video", (req, res) => {
   // Name is the token of the transmission
   // Path is the location of the flv file
-  const {name, path} = req.query;
+  const { name } = req.query;
+  const vidPath = path.resolve(recPath + req.query.path.split("/").pop());
+
+  // TODO: Sends data to firestore that the video is being processed and uploaded
 
   // Runs the shell script
-  shelljs.exec(`convert.sh ${path}`, (code, stdout, stderr) => {
+  shelljs.exec(`${__dirname}/convert.sh ${vidPath}`, (code, stdout, stderr) => {
     if (code !== 0) return res.sendStatus(500);
 
     // Gets the mp4 file
-    const newFile = stdout;
+    const newFile = path.resolve(stdout).trim();
 
-    // TODO: Send to bucket
-    console.log(newFile);
+    const uploadOpt = {
+      validation: "crc32c",
+    }
 
-    
+    // Upload file to bucket
+    bucket.upload(newFile, uploadOpt, (err, file) => {
+      if (err) {
+        console.log(`Uploading ${newFile} to bucket failed!\n ${err}`);
+      } else {
+        console.log(`Uploaded ${newFile} to bucket!`);
+      }
 
-    // Delete file
-    shelljs.rm(newFile);
+      // Delete file after uploading
+      shelljs.rm(newFile);
+    });
 
     return res.sendStatus(201);
   })
