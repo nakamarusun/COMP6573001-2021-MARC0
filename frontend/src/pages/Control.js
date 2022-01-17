4f7769ba48675ebc7a3f5e2000f72b01375710c2
nakamarusun <nakamarujc@gmail.com>
2022-01-17 10:23:04 +0700

    Modified control to have a textbox to connect to the peer

--- frontend/src/pages/Control.js
+++ frontend/src/pages/Control.js
@@ -5,37 +5,41 @@ import { useEffect, useState } from 'react';
 const getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
 const Control = () => {
-
-    const [peer, setPeer] = useState(new Peer(
-        Math.random().toString(36).slice(2),
-        {
-            host: "marc0.jasoncoding.com",
-            port: 9003,
-            path: "/marc0webrtc",
-            secure: false
-        }
-    ));
-
-    useEffect(() => {        
-        getUserMedia({video: true, audio: true}, (stream) => {
-            const call = peer.call("", stream);
-            call.on("stream", (remoteStream) => {
-
-            });
-        }, (err) => {
-            console.log("Error getting stream\n" + err);
-        });
-    })
-
-    return ( 
-        <div className="w-full h-full">
-            <div className="max-w-5xl h-2/5 w-4/5 m-auto text-center flex flex-col items-center justify-center divide-y">
-                <div className="bg-blue-crayola w-full h-full">bruh</div>
-                <input type="text" id="fname" name="fname" /><br />
-                <input type="submit" value="Submit" />
-            </div>
+  
+  const [peer] = useState(new Peer(
+    Math.random().toString(36).slice(2),
+    {
+      host: "marc0.jasoncoding.com",
+      port: 9003,
+      path: "/marc0webrtc",
+      secure: false
+    }
+  ));
+  
+  // When done testing, 
+  function callMarc1() {
+    getUserMedia({video: true, audio: true}, (stream) => {
+      const call = peer.call("", stream);
+      // TODO: Get marc1's peer id somehow
+      call.on("MARC1ID", (remoteStream) => {
+        
+      });
+    }, (err) => {
+      console.log("Error getting stream\n" + err);
+    });
+  }
+  
+  return ( 
+    <div className="w-full h-full">
+      <div className="max-w-5xl h-full w-4/5 m-auto text-center flex flex-col items-center justify-start divide-y">
+        <div className="h-2/5 bg-blue-crayola w-full">bruh</div>
+        <div className="flex flex-row space-x-2 mt-2">
+          <input className="w-24 bg-gray-300 rounded-sm" type="text" id="fname" name="fname" /><br />
+          <button className="bg-blue-crayola text-white px-2 py-1 rounded-sm" onClick={callMarc1}>Connect</button>
         </div>
+      </div>
+    </div>
     );
-}
-
+  }
+    
 export default Control;
