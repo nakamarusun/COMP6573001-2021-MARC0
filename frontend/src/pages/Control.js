cb8a9022096f3aa68cf6ac49859427987259a4f0
bentlyedyson <56628774+bentlyedyson@users.noreply.github.com>
2022-01-25 20:02:13 +0700

    control.js
    responsive complete

--- frontend/src/pages/Control.js
+++ frontend/src/pages/Control.js
@@ -131,25 +131,25 @@ const Control = () => {
               setText(a.target.value);
             }} /><br />
             <button className="bg-blue-crayola text-white px-2 py-1" onClick={callMarc1}>Connect</button> */}
-            <div><i class="fas fa-play-circle md:text-6xl lg:text-4xl"></i></div>
-            <div><i class="fas fa-stop md:text-6xl lg:text-4xl"></i></div>
+            <div><i class="fas fa-play-circle text-4xl md:text-6xl lg:text-4xl"></i></div>
+            <div><i class="fas fa-stop text-4xl md:text-6xl lg:text-4xl"></i></div>
           </div>
           {/*turnL /* up /* turn right*/ }
             <div className="flex felx-row justify-center">
-              <div><i class="fas fa-angle-double-left text-green-500 md:text-8xl lg:text-7xl mt-2 mr-4 "></i></div>
-          <div><i onClick={handleUp} class="fas fa-caret-square-up md:text-8xl lg:text-7xl pb-4 mt-2"></i></div>
-          <div><i class="fas fa-angle-double-right text-orange-500 md:text-8xl lg:text-7xl mt-2 ml-4 "></i></div>
+              <div><i class="fas fa-angle-double-left text-6xl  text-green-500 md:text-8xl lg:text-7xl mt-2 mr-4 "></i></div>
+          <div><i onClick={handleUp} class="fas fa-caret-square-up text-6xl md:text-8xl lg:text-7xl pb-4 mt-2"></i></div>
+          <div><i class="fas fa-angle-double-right text-6xl  text-orange-500 md:text-8xl lg:text-7xl mt-2 ml-4 "></i></div>
 
           </div>
           <div className="flex flex-row justify-center ">
             {/* left */}
-            <div><i onClick={handleLeft} class="fas fa-caret-square-left text-blue-800 md:text-8xl lg:text-7xl "></i></div>
+            <div><i onClick={handleLeft} class="fas fa-caret-square-left text-6xl  text-blue-800 md:text-8xl lg:text-7xl "></i></div>
             {/* stop */}
-            <div><i class="fas fa-stop-circle text-red-600 md:text-8xl lg:text-7xl px-4 pb-4  "></i></div>
+            <div><i class="fas fa-stop-circle  text-6xl text-red-600 md:text-8xl lg:text-7xl px-4 pb-4  "></i></div>
             {/* right */}
-            <div><i onClick={handleRight} class="fas fa-caret-square-right text-yellow-500 md:text-8xl lg:text-7xl "></i></div>
+            <div><i onClick={handleRight} class="fas fa-caret-square-right text-6xl text-yellow-500 md:text-8xl lg:text-7xl "></i></div>
           </div>
-          <div><i onClick={handleDown} class="fas fa-caret-square-down text-pink-600 md:text-8xl lg:text-7xl "></i></div>
+          <div><i onClick={handleDown} class="fas fa-caret-square-down text-6xl text-pink-600 md:text-8xl lg:text-7xl "></i></div>
         </div>
       </div>
     </div>
