9805d08ad273e72bdd8751d4ac75b253404972ab
BryanPutra <bpsuandi@gmail.com>
2022-01-26 14:12:41 +0700

    listen to take notes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <AuthProvider>
       <Router>
         <Routes>
-          <Route path="/mainmenu"
+          {/* <Route path="/mainmenu"
             element={
               <ProtectedRoute>
                 <Marc1IsPairedRoute>
@@ -52,7 +52,7 @@ function App() {
                   <Control />
                 </Marc1IsPairedRoute>
               </ProtectedRoute>
-            } />
+            } /> */}
           <Route path="/"
             element={
               <ProtectedRoute>
@@ -71,11 +71,11 @@ function App() {
               </ProtectedRoute>
             }
           />
+          {/* use these for testing purposes and comment the protected routes */}
           {/* <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} /> */}
-          {/* use these for testing purposes and comment the protected routes */}
-          {/* <Route path="/control" element={<Control />} />
-          <Route path="/mainmenu" element={<MainMenu />} /> */}
+          <Route path="/mainmenu/control" element={<Control />} />
+          <Route path="/mainmenu" element={<MainMenu />} />
         </Routes>
       </Router>
     </AuthProvider>
